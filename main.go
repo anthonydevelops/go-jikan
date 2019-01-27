f6b1bde2b6d61fc5e268307b8b556810784edcf1
anthonydevelops <anthonydevelops@gmail.com>
2019-01-26 18:10:10 -0800

    Updated with Colly web scraper

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
@@ -11,7 +14,19 @@ func main() {
 	flag.Parse()
 
 	reader := *pages
-	fmt.Println(reader)
+	s := strings.Split(reader, ",")
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("https://en.wikipedia.org/" + "wiki/" + s[0])
 }
 
 func checkError(err error) {
