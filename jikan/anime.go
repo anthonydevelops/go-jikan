d2186a2ea50666bea2321d1818dc269b7020fb35
anthonydevelops <anthonydevelops@gmail.com>
2019-01-27 23:58:45 -0800

    Updated with new entry point

---
+++ jikan/anime.go
@@ -0,0 +1,10 @@
+package anime
+
+import "fmt"
+
+const baseURL = "https://api.jikan.moe/v3"
+
+func anime() {
+	// This is where i'll bring the whole application together
+	fmt.Println("To be continued...")
+}
